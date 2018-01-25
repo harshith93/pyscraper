399305baf7cc52883eb7486a03f9ae11d1b427b5
Harshith Potu <harshith@24-124-102-131-static.midco.net>
2018-01-24 23:16:59 -0600

    Able to retrieve images into a list

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 import certifi
 import urllib3
 import time
+from bs4 import BeautifulSoup
+import re
 
 url = "https://www.flickr.com/search/?text=paris"
 
@@ -22,17 +24,27 @@ class Scraper(object):
         f = open('source.html', 'w')
         f.write(self.crawl().decode())
         f.close()
-        return
+        with open('source.html') as fp:
+            soup = BeautifulSoup(fp, 'html.parser')
+            return [element.text for element in soup.find_all("script")]
 
-scraped = Scraper(url)
-scraped.write()
+    def find_images(self):
+        scripts = self.write()
+        images = []
+        for script in scripts:
+            images_sub = re.findall('([-\w]+\.(?:jpg))', script)
+            images += images_sub
+        return images
+
+    def find_image_num(self):
+        images = self.find_images()
+        image_numbers = [image.split('_')[0] for image in images]
+        print(image_numbers)
 
 
 start = time.clock()
-if "2163641698" in open('source.html').read():
-    print(True)
-else:
-    print(False)
+scraped = Scraper(url)
+scraped.find_image_num()
 print(time.clock() - start)
 
 print('Done')
