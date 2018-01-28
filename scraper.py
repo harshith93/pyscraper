6cd6efeddfc2618957147142c83d80889faeb581
Harshith Potu <harshith@24-124-102-131-static.midco.net>
2018-01-27 23:53:50 -0600

    Testing nested pools

--- scraper.py
+++ scraper.py
@@ -10,32 +10,35 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 import time
+from multiprocessing import Pool
+import multiprocessing
 import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
+import pickle
+
 
 class Scraper(object):
     """docstring for Scraper."""
     def __init__(self):
         super(Scraper, self).__init__()
-        # city = city.replace(' ', '%20')
-        # self.url = "https://www.flickr.com/search/?text=" + city
-        # print(self.url)
 
-    def crawl(self):
+
+    def crawl(self, url):
         http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
                                     ca_certs=certifi.where())
-        response = http.request('GET', self.url)
-        result = response.data
+        response = http.request('GET', url)
+        try:
+            result = response.data.decode()
+        except Exception as e:
+            print("Results no")
+            print(e)
         return result
 
-    def write(self):
-        f = open('source.html', 'w')
-        f.write(self.crawl().decode())
-        f.close()
-        with open('source.html') as fp:
-            soup = BeautifulSoup(fp, 'html.parser')
-            return [element.text for element in soup.find_all("script")]
+    def get_info_page(self, url):
+        with self.crawl(url) as source:
+            # soup = BeautifulSoup(fp, 'html.parser')
+            print(url)
 
     def find_images(self):
         scripts = self.write()
@@ -56,11 +59,8 @@ class Scraper(object):
         last_height = browser.execute_script("return document.body.scrollHeight")
         print(last_height)
         while True:
-            # Scroll down to bottom
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            # Wait to load page
             time.sleep(SCROLL_PAUSE_TIME)
-            # Calculate new scroll height and compare with last scroll height
             new_height = browser.execute_script("return document.body.scrollHeight")
             print(new_height)
             if new_height == last_height:
@@ -75,54 +75,48 @@ class Scraper(object):
 
 
     def sel_scrape(self, city):
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        browser = webdriver.Chrome()
-        browser.get("https://www.flickr.com/search/?text=" + city)
-        self.browser_scroll(browser)
-        elements = browser.find_elements_by_css_selector('.photo-list-photo-interaction .overlay')
-        for element in elements:
-            print(element.get_attribute("href"))
 
-
-def get_link(element):
-    return element.get_attribute("href")
-
-
-class ImageInfo(object):
-    """docstring for ImageInfo."""
-    def __init__(self):
-        super(ImageInfo, self).__init__()
-
-    def grab_info(self, element):
-        url = element.get_attribute("href")
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         browser = webdriver.Chrome(chrome_options=chrome_options)
-        browser.get(url)
-        image = browser.find_element_by_class_name('main-photo')
-        print(image.get_attribute('src'))
+        print("https://www.flickr.com/search/?text=" + city)
+        browser.get("https://www.flickr.com/search/?text=" + city)
+        self.browser_scroll(browser)
+        elements = browser.find_elements_by_css_selector('.photo-list-photo-interaction .overlay')
+        return [element.get_attribute("href") for element in elements]
 
+
+    def get_info_city(self, city):
+        links_list = self.sel_scrape(city)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            try:
+                futures = [executor.submit(self.image_info, link) for link in links_list]
+                for future in as_completed(futures):
+                    print(future.result())
+            except Exception as e:
+                print(e)
+
+    def image_info(self, link):
+        self.get_info_page(link)
+        return 1
+
+    def get_link(self, element):
+        return element.get_attribute('href')
 
 cities = input("Please enter the names of the cities separated by comma: ")
-#cities = [city for city in cities.split(',')]
-#cities = [Scraper("%s" %(city)) for city in cities]
+cities = [city for city in cities.split(',')]
+cities = [city.replace(' ', '%20') for city in cities]
 print("Processing...")
 
 NUM_WORKERS = 4
 start_time = time.time()
-futures = None
+# scraped = Scraper()
+# print(scraped.scrape_cities(cities))
 with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
-    futures = {executor.submit(Scraper().sel_scrape, city.replace(' ', '%20')) for city in cities.split(',')}
+    futures = [executor.submit(Scraper().get_info_city, city) for city in cities]
     concurrent.futures.wait(futures)
 
 end_time = time.time()
-
-
-# NUM_WORKERS = 10
-# with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
-#     futures = {executor.submit(Scraper().sel_scrape, city.replace(' ', '%20')) for city in cities.split(',')}
-#     concurrent.futures.wait(futures)
 
 print("Time for Concurrent: %s secs" % (end_time - start_time))
 print('Done')
